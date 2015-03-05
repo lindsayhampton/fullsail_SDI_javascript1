14f87f638ac93b7e46567635f6407adc5a04442c
lindsayhampton <lrhampton@fullsail.edu>
2015-03-05 15:42:13 -0500

    Basic variables.
    Learning about declaring & defining variables.

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -14,6 +14,31 @@
 
 // Single line comment. ONLY this line will be ignored.
 // Alert box - pop up box that will alert the user with information
-
 alert("Text that the user will see.");
+
+// Console.log - Shows information to the programmer
+// Great for checking values and debugging.
+// Also shows all errors.
+console.log("This is the console.");
+
+// Declare a variable
+// Use the keyword of var
+var whatever1;
+
+// Define the variable
+// Sets the value of the existing variable
+whatever1 = 42;
+
+// Console.log the variable
+console.log("The variable whatever1 is");
+console.log(whatever1);
+
+//Declare and define a variable at the same time.
+var a = 2;
+
+console.log(a);
+
+a+3;
+a=a+3;
+console.log(a);
 
