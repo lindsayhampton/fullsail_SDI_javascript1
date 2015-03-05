3d70603c911a8bcc881cddbe2c5a34636f4dd98c
lindsayhampton <lrhampton@fullsail.edu>
2015-03-05 18:32:19 -0500

    Created HTML & Java Documents
    Created HTML & Java Documents, created and tested alert to test link.

---
+++ Day 2/Lab Assignment Day 2/js/script.js
@@ -0,0 +1,8 @@
+/*
+    Lindsay Hampton
+    March 5, 2015
+    Section 00
+    Day 2 Lab Assignment
+ */
+
+//alert("Testing 1, 2, 3...");
--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -176,4 +176,4 @@ var pi = 3.14;
 var num= 5.5678912345;
 // Round 2 decimal places
 var n = num .toFixed(2);
-console.log(n);
+console.log("The rounded number to 2 decimal places is "+n);
