9b8cab2661bc956ff66f73f4614a872c28de9060
lindsayhampton <lrhampton@fullsail.edu>
2015-03-05 16:56:27 -0500

    Strings, PEMDAS, Booleans, OH MY!
    Strings, PEMDAS, Booleans, OH MY!

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -86,3 +86,72 @@ console.log(remainder);
 var evenOrOdd = 67%2;
 console.log(evenOrOdd);
 
+// Assignment operators
+/*
+"=" Assigns the value to the variable
+"++" Adds 1 to the current value of the variable
+"--" Subtracts 1 from the current value of the variable
+"+=" Adds the number to the current value of the variable
+"-=" Subtracts the number from the current value of the variable
+"/=" Divide the current value of the variable by #
+"*=" Multiply the current value by #
+ */
+
+var assign = 0;
+// ++ is the same as assign = assign+1;
+assign++;
+console.log(assign);
+
+// -- is the same as assign = assign-1;
+assign--;
+console.log(assign);
+
+// += is the same as assign = assign + #;
+assign+=5; // assign = assign + 5;
+console.log(assign);
+
+// -= is the same as assign = assign - #;
+assign-=2;
+console.log(assign);
+
+// /= is the same as assign = assign/#;
+assign/=3;
+console.log(assign);
+
+// *= is the same as assign = assign * #;
+assign*=7;
+console.log(assign);
+
+// strings - any text that we use
+// quotes are needed to distinguish between variables and plain text
+
+var kermit = "light green";
+var frogName = "kermit";
+console.log(frogName);
+
+// To double quote or not to double quote
+// Can use an escape character \ backslash in front of '
+var phrase = 'I don\'t know';
+console.log(phrase);
+
+// Escape character can also do multiple lines \n - new line character
+var phrase2 = "I don't know. \n You never do!";
+console.log(phrase2);
+
+// Boolean - kind of like a light switch
+// Either true or false
+// NOT a text string!!! Must be lowercase!!!
+var yes = true;
+var nope = false;
+
+// Order of Operations
+// PEMDAS
+
+// Find the average quiz grade
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 90;
+
+var average = (quiz1 + quiz2 + quiz3)/3;
+console.log(average);
+
