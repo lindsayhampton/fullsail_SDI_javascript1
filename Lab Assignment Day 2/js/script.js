3a1bab8771a77a44bb76a01464f82ea892d8d9bd
lindsayhampton <lrhampton@fullsail.edu>
2015-03-05 18:51:18 -0500

    Completed problem 1
    Completed problem 1

--- Lab Assignment Day 2/js/script.js
+++ Lab Assignment Day 2/js/script.js
@@ -6,3 +6,19 @@
  */
 
 //alert("Testing 1, 2, 3...");
+
+// Slices of Pie pt. 1
+
+    // Number of people at the party
+var numPartygoers = 10;
+
+    // Number of pizzas ordered
+var numPizza = 5;
+
+    // Number of slices per pizza
+var numTotalSlices = 8;
+
+    // Number of slices per person
+var numSlices = (numPizza * numTotalSlices)/ numPartygoers;
+console.log("Each person at the party will recieve "+numSlices+" slices of pizza.");
+
