b515051e9e7eacde938e54ce522b7e0a5f6b5410
lindsayhampton <lrhampton@fullsail.edu>
2015-03-05 14:54:12 -0500

    New Day 2 Intro Files
    Created HTML & JS files for Day 2.

---
+++ Day 2/js/script.js
@@ -0,0 +1,19 @@
+/*
+    Lindsay Hampton
+    March 5, 2015
+    Section 00
+    Day 2 Intro
+*/
+
+//alert("Testing to see if connected");
+
+/*
+    Example of a multilined comment
+    Anything inside of here will not be seen by the computer.
+ */
+
+// Single line comment. ONLY this line will be ignored.
+// Alert box - pop up box that will alert the user with information
+
+alert("Text that the user will see.");
+
