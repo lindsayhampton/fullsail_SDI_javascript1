0d346c9be61e2a7f6578ed2f788ba99bd868ff0c
lindsayhampton <lrhampton@fullsail.edu>
2015-03-07 14:43:09 -0500

    Learned basic arrays
    Learned basics of arrays, met the Avengers and launched the Hulk into
    space.

--- Day 3/js/day3Arrays.js
+++ Day 3/js/day3Arrays.js
@@ -7,3 +7,41 @@
 
 //alert("Testing 1, 2, 3...");
 
+// Create a basic array
+var avengersNames = ["Thor", "Hulk", "Iron Man", "Captain America"];
+
+// Print out whole array
+console.log(avengersNames);
+
+// Print out one specific item in the array
+console.log(avengersNames[2]);
+
+// Swap items in an array
+avengersNames[1] = "Black Widow";
+console.log(avengersNames[1]);
+
+// Use a variable for the index number
+var num = 3;
+console.log(avengersNames[num]);
+
+// Find the length of an array
+// Length property - how many items are in the array.
+// Dot syntax - fancy use of a .
+console.log(avengersNames.length);
+
+// Add an item to our array
+avengersNames[4] = "Hawkeye";
+avengersNames[avengersNames.length] = "Wolverine";
+console.log(avengersNames);
+
+
+// Picking Oranges
+var orangesPicked = [13, 350, 1000];
+
+// How many oranges did we pick in total?
+var totalOranges = orangesPicked[0] + orangesPicked[1] + orangesPicked[2];
+console.log("The total number of oranges picked is "+totalOranges+".");
+
+var averageOranges = totalOranges/3;
+console.log("The average number of oranges picked per day is "+averageOranges+".");
+
