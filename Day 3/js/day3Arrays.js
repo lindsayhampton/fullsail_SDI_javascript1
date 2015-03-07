2c38763af32400c58db4f45ac72b1e380c3607c4
lindsayhampton <lrhampton@fullsail.edu>
2015-03-07 15:28:30 -0500

    Created Prompting File
    Prompts on prompts on prompts!

--- Day 3/js/day3Arrays.js
+++ Day 3/js/day3Arrays.js
@@ -1,8 +1,8 @@
 /*
-    Lindsay Hampton
-    March 7, 2015
-    Section 00
-    Day 3 - Arrays
+ Lindsay Hampton
+ March 7, 2015
+ Section 00
+ Day 3 - Arrays
  */
 
 //alert("Testing 1, 2, 3...");
---
+++ Day 3/js/day3Prompts.js
@@ -0,0 +1,27 @@
+/*
+ Lindsay Hampton
+ March 7, 2015
+ Section 00
+ Day 3 - Prompts
+ */
+
+//alert("Testing 1, 2, 3...");
+
+// Ask the user for input
+// Prompt("Text that the user will see");
+
+var userInput = prompt("Enter year of birth:");
+console.log(userInput);
+
+// Calculating the area of a rectangle
+// Length*Width
+
+// Prompt the user for the width & height
+var length = prompt("Let's calculate the area of a rectangle! \nPlease enter the rectangle's length:");
+var width = prompt("Please enter the rectangle's width:");
+
+// Calculate the area
+var area = length*width;
+
+//Tell the user the answer
+console.log("The area of a rectangle with a length of "+length+" and a width of "+width+" is "+area+".");
