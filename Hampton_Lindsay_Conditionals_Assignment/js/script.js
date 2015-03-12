9e74f24406df18c42c04ab0b374987e0a3731283
lindsayhampton <lrhampton@fullsail.edu>
2015-03-12 18:58:33 -0400

    First statement
    PUPPIES EVERYWHERE.

--- Hampton_Lindsay_Conditionals_Assignment/js/script.js
+++ Hampton_Lindsay_Conditionals_Assignment/js/script.js
@@ -7,3 +7,13 @@
 
 //alert("Testing 1, 2, 3...");
 
+
+// How much profit will you make on your dog's puppies?
+
+// How many puppies did your dog have?
+var sellingPuppies = prompt("We are going to calculate how much of a profit you will make by selling your dog's puppies! \n How many puppies did your dog have?");
+if (sellingPuppies=="" || isNaN(sellingPuppies)){
+    sellingPuppies = prompt("Please use numbers to enter how many puppies your dog had.\n This is a required field.")
+}
+//console.log(sellingPuppies);
+
