6735416cd3845fc0fdd006e3c50e28ae479aabd2
lindsayhampton <lrhampton@fullsail.edu>
2015-03-12 19:35:17 -0400

    Finished!
    How much will you make on your puppies? (Hint: probably nothing because
    you’re going to keep them all.)

--- Hampton_Lindsay_Conditionals_Assignment/js/script.js
+++ Hampton_Lindsay_Conditionals_Assignment/js/script.js
@@ -30,39 +30,45 @@ if (areYou === "no"){
 }
 
 // How many puppies did your dog have?
-var sellingPuppies = prompt("How many puppies did your dog have?");
-if (sellingPuppies=="" || isNaN(sellingPuppies)){
-    sellingPuppies = prompt("Please use numbers to enter how many puppies your dog had.\n This is a required field.")
-}
+if (areYou === "yes") {
+    var sellingPuppies = prompt("How many puppies did your dog have?");
+    if (sellingPuppies == "" || isNaN(sellingPuppies)) {
+        sellingPuppies = prompt("Please use numbers to enter how many puppies your dog had.\n This is a required field.")
+    }
+
 //console.log(sellingPuppies);
 
 // Are you going to keep any of the puppies?
-var keep = prompt("Puppies are super cute! \n How many of your dog's puppies are you going to keep for yourself?");
-if (keep =="" || isNaN(keep)){
-    keep = prompt("Please use numbers to enter how many of your dogs puppies you plan to keep. \n This is a required field.");
-}
+    var keep = prompt("Puppies are super cute! \n How many of your dog's puppies are you going to keep for yourself?");
+    if (keep == "" || isNaN(keep)) {
+        keep = prompt("Please use numbers to enter how many of your dogs puppies you plan to keep. \n This is a required field.");
+    }
+
 
 // How much are you charging per puppy?
-var cost = prompt("How much money are you charging per puppy that you sell? \n Please enter numbers only.");
-if (cost == "" || isNaN(cost)){
-    cost = prompt("Please use numbers to enter how much you are charging per puppy \n Do not include the dollar sign.");
-}
+    var cost = prompt("How much money are you charging per puppy that you sell? \n Please enter numbers only.");
+    if (cost == "" || isNaN(cost)) {
+        cost = prompt("Please use numbers to enter how much you are charging per puppy \n Do not include the dollar sign.");
+    }
+
 
 // Vet costs
-var expenses = prompt("How much money have you spent on these puppies so far?\n Please enter numbers only.");
-if (expenses == "" || isNaN(expenses)){
-    expenses = prompt("Please use numbers to enter how much your expenses have been on these puppies so far \n Do not include the dollar sign.");
-}
+    var expenses = prompt("How much money have you spent on these puppies so far?\n Please enter numbers only.");
+    if (expenses == "" || isNaN(expenses)) {
+        expenses = prompt("Please use numbers to enter how much your expenses have been on these puppies so far \n Do not include the dollar sign.");
+    }
+
 
 // Results
-var puppiesSold = sellingPuppies - keep;
-console.log("You are selling "+puppiesSold+" puppies for profit.");
+    var puppiesSold = sellingPuppies - keep;
+    console.log("You are selling " + puppiesSold + " puppies for profit.");
 
-var puppyProfit = puppiesSold*cost;
-var totalProfit = puppyProfit - expenses
+    var puppyProfit = puppiesSold * cost;
+    var totalProfit = puppyProfit - expenses
 
-if (puppyProfit <= totalProfit){
-    console.log("You have not made a profit on these puppies. You have lost "+totalProfit+" dollars in expenses.");
-} else {
-    console.log("You have made a profit of $"+totalProfit+" by selling these puppies!");
+    if (expenses >= totalProfit) {
+        console.log("You have not made a profit on these puppies. You have lost " + totalProfit + " dollars in expenses.");
+    } else {
+        console.log("You have made a profit of $" + totalProfit + " by selling these puppies!");
+    }
 }
