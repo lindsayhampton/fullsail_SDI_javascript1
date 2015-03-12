79d86f199e043b58e88dc5a581d3e3085c83299c
lindsayhampton <lrhampton@fullsail.edu>
2015-03-12 15:03:01 -0400

    Learned about nesting if statements
    Please don’t make me see 50 shades of gray.

--- Day 5/js/day5NestingIfs.js
+++ Day 5/js/day5NestingIfs.js
@@ -6,3 +6,47 @@
  */
 
 //alert("Testing 1, 2, 3...");
+
+// Some decisions affect other decisions
+
+// If it is warm, then lets go to the beach. If it is not, then lets go to the movies.
+
+// Get temperature
+var temp = 66;
+
+// Set water temperature
+var waterTemp = 78;
+
+if(temp>=80){
+    console.log("Let's go to the beach!");
+    if (waterTemp>75){
+        console.log("Let's go swimming!");
+    } else {
+        console.log("Let's get a tan!");
+    }
+} else {
+    console.log("Let's go to the movies!");
+
+    // Do we have any kids in the group?
+    var kids = "no";
+
+    // Test if we have kids
+    if(kids==="no"){
+        console.log("Let's go see 50 Shades of Gray!");
+    } else {
+        console.log("Let's go see the Spongebob Squarepants Movie!");
+    }
+
+
+}
+
+/*
+// If the water temp is above 75, lets go swimming. If not, lets get a tan.
+
+if (waterTemp>75){
+    console.log("Let's go swimming!");
+} else {
+    console.log("Let's get a tan!");
+}
+*/
+
