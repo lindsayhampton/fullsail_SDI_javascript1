feb9cefa5c31e7d43ec628a6c898ae71997e1557
lindsayhampton <lrhampton@fullsail.edu>
2015-03-14 18:37:16 -0400

    The song that never ends!
    It just goes on and on, my friends.

--- loops_worksheet/js/whileLoop.js
+++ loops_worksheet/js/whileLoop.js
@@ -2,7 +2,15 @@
  Lindsay Hampton
  March 14, 2015
  Section 00
- Loops Worksheet
+ Loops Worksheet - While Loop Example
  */
 
-alert("Testing 1, 2, 3...");
+//alert("Testing 1, 2, 3...");
+
+var counter = 0;
+
+while(counter<1000){
+    console.log("This is the song that never ends,\nIt just goes on and on my friends.");
+    console.log("Some people started singing it not knowing what it was\n And they'll continue singing it forever just because...");
+    counter++;
+}
