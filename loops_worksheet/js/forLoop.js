a5feee28b6fe1fb24545ec5e7d5b8043ec359632
lindsayhampton <lrhampton@fullsail.edu>
2015-03-14 19:04:10 -0400

    Completed For Loops
    Cars for sale! Get your cars here!

--- loops_worksheet/js/forLoop.js
+++ loops_worksheet/js/forLoop.js
@@ -6,3 +6,11 @@
  */
 
 // alert("Testing 1, 2, 3...");
+
+// Cars at the Car Dealership
+var carsOnLot = ["Honda Civic", "Pontiac Firebird", "Dodge Ram", "Chevrolet Camaro", "Hyundai Tiburon", "Kia Sportage", "Toyota Camry"];
+
+console.log("The following cars are available for sale on our lot right now:");
+for(var i = 0; i < carsOnLot.length; i++){
+    console.log(carsOnLot[i]);
+}
