a9ff58ca7d0ce1c85ec310b64f9f824b9a8ceba5
lindsayhampton <lrhampton@fullsail.edu>
2015-03-14 18:47:48 -0400

    Do While Loops
    That’s an obscene amount of beer that you’re drinking in such a short
    amount of time.

---
+++ loops_worksheet/js/doWhileLoop.js
@@ -0,0 +1,17 @@
+/*
+ Lindsay Hampton
+ March 14, 2015
+ Section 00
+ Loops Worksheet - Do While Loop
+ */
+
+//alert("Testing 1, 2, 3...");
+
+var i = 99;
+
+do {
+    console.log(i+" bottles of beer on the wall, "+i+" bottles of beer... You take one down, pass it around, "+i+" bottles of beer on the wall!");
+    i--;
+} while(i>0);
+
+console.log("No more bottles of beer on the wall, no more bottles of beer... You go to the store to buy some more, 99 bottles of beer on the wall!")
