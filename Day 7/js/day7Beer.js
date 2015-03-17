b0b7ceea7219a1372925c9ab8483e21a3c6caa71
lindsayhampton <lrhampton@fullsail.edu>
2015-03-17 13:52:44 -0400

    How many bottles of beer on the wall?
    This feels a little redundant, almost like deja vu.

--- Day 7/js/day7Beer.js
+++ Day 7/js/day7Beer.js
@@ -5,4 +5,17 @@
  Day 7 - Beer
  */
 
-alert("Testing 1, 2, 3...");
+//alert("Testing 1, 2, 3...");
+
+// Code the song 99 Bottles of beer on the wall.
+
+//Basic structure of for loop
+// For (var i =0; condition to test, increment of change){}
+
+for (var i = 99; i > 0; i--){
+    if (i===1){
+        console.log(i + " bottle of beer on the wall. " + i + " bottle of beer. You take one down and pass it around, no more bottles of beer on the wall.");
+    } else {
+        console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. You take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.")
+    }
+}
