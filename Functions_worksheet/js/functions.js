e53129b6747417464c4c57a001a22d1c1245dbab
lindsayhampton <lrhampton@fullsail.edu>
2015-03-17 18:11:45 -0400

    Created the function for problem 1
    Created the function to calculate the circumference of a circle

--- Functions_worksheet/js/functions.js
+++ Functions_worksheet/js/functions.js
@@ -7,3 +7,10 @@
 
 //alert("Testing 1, 2, 3...");
 
+// Calculate the circumference of a circle
+// 2 pi r
+
+function circCirc(r) {
+    var circumference = 2*Math.PI*r
+    console.log("The circumference of the circle is "+circumference);
+}
