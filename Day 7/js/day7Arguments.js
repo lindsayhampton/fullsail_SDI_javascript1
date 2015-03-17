1ee6c0f272705d9416f927c0a404062ed4129f44
lindsayhampton <lrhampton@fullsail.edu>
2015-03-17 16:30:32 -0400

    Finished arguments
    3… 2… 1… FIGHT

--- Day 7/js/day7Arguments.js
+++ Day 7/js/day7Arguments.js
@@ -6,3 +6,34 @@
  */
 
 //alert("Testing 1, 2, 3...");
+
+// Arguments - Goes into the functions - part of the function call
+// Parameters - catch the arguments and in the function definition
+
+// Create a function that calculates the area of a rectangle... again!
+// Add in parameters
+function calcArea(w,h){
+    //var width =10;
+    //var height =20;
+    var area =w*h;
+    console.log("The area is "+area);
+}
+
+// Call our function
+// Add arguments to the function call
+calcArea(10,20);
+calcArea(20,40);
+
+var width=prompt("Enter a width");
+var height=prompt("Enter a height");
+calcArea(width,height)
+
+// Dog Years
+function dogYears(humanAge){
+    // Dog Years = human years * 7;
+    var dogAge = humanAge*7;
+    console.log("Your age in dog years is "+dogAge);
+}
+
+// Call the dog function
+dogYears(4);
