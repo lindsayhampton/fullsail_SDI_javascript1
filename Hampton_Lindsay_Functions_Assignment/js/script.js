4169dcce5bfe9bc79575e2917206dbcbaab66b24
lindsayhampton <lrhampton@fullsail.edu>
2015-03-25 13:34:08 -0400

    Completed worksheet!
    Added final function, fixed errors, and tested thoroughly.

--- Hampton_Lindsay_Functions_Assignment/js/script.js
+++ Hampton_Lindsay_Functions_Assignment/js/script.js
@@ -10,9 +10,13 @@
 // Finding the cost of a product plus tax.
 
 // Create variable for original price
+
 var origPrice = prompt("Let's calculate the total price of your next purchase.\nWhat is the original price of the product?\nPlease leave out the dollar sign.");
 origPrice = parseInt(origPrice);
 
+var yesFood = function () {
+    console.log("Because this is a food item, it will not be taxed.\nThe cost of your item is $" +origPrice+ ".");
+};
 
 // Validate the variable prompt.
 
@@ -24,39 +28,57 @@ while (isNaN(origPrice) || origPrice===""){
     }
 }
 
-// Create a variable for tax percentage.
-var taxAmount = prompt("Please enter the percentage of sales tax.");
-taxAmount = parseInt(taxAmount);
-var divide = 100;
+var foodItem = prompt("Is this product a food item?");
+foodItem = foodItem.toLowerCase();
 
-// Validate
-
-while (isNaN(taxAmount) || taxAmount===""){
-    if (isNaN(taxAmount)){
-        taxAmount = prompt("Please only enter numbers.\nWhat is the percentage of sales tax?");
-    } else if (taxAmount===""){
-        taxAmount = prompt("This field is required, please do not leave it blank.\nWhat is the original price of the product?");
+while (foodItem!=="yes" && foodItem!=="no" || foodItem===""){
+    if(foodItem!=="yes" && foodItem!=="no"){
+        foodItem = prompt("Please only enter yes or no.\nIs this product a food item?")
+    } else if(foodItem===""){
+        foodItem = prompt("This field is required, please do not leave it blank.\nIs this product a food item?");
     }
 }
 
+foodItem = foodItem.toLowerCase();
+
+if (foodItem==="no") {
+
+// Create a variable for tax percentage.
+    var taxAmount = prompt("Please enter the percentage of sales tax.");
+    taxAmount = parseInt(taxAmount);
+    var divide = 100;
+
+// Validate
+
+    while (isNaN(taxAmount) || taxAmount === "") {
+        if (isNaN(taxAmount)) {
+            taxAmount = prompt("Please only enter numbers.\nWhat is the percentage of sales tax?");
+        } else if (taxAmount === "") {
+            taxAmount = prompt("This field is required, please do not leave it blank.\nWhat is the original price of the product?");
+        }
+    }
+
 // Create function
-function taxTotal(origPrice, taxAmount,divide){
-    var taxCash = (origPrice*taxAmount)/divide;
-    return taxCash;
-}
+    function taxTotal(origPrice, taxAmount, divide) {
+        var taxCash = (origPrice * taxAmount) / divide;
+        return taxCash;
+    }
 
 // Returned
-var taxCash = taxTotal(origPrice, taxAmount, divide);
-console.log("The amount in tax you will be paying is $"+taxCash+".");
+    var taxCash = taxTotal(origPrice, taxAmount, divide);
+    console.log("The amount in tax you will be paying is $" + taxCash + ".");
 
 
 // Function 2
-function price(taxCash, origPrice){
-    var totalPrice = taxCash+origPrice
-    return totalPrice;
-}
+    function price(taxCash, origPrice) {
+        var totalPrice = taxCash + origPrice;
+        return totalPrice;
+    }
 
 // Returned
-var totalPrice = price(taxCash, origPrice);
-console.log("The total amount you will pay for this item is $"+totalPrice+".");
+    var totalPrice = price(taxCash, origPrice);
+    console.log("The total amount you will pay for this item is $" + totalPrice + ".");
 
+} else if (foodItem==="yes") {
+    yesFood();
+}
