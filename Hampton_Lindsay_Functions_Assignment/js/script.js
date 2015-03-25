d90e425c7020786f8d39feb32c960b5ca6c6a5c5
lindsayhampton <lrhampton@fullsail.edu>
2015-03-25 10:49:36 -0400

    Created variables for calculator.
    Created variables for calculator.

--- Hampton_Lindsay_Functions_Assignment/js/script.js
+++ Hampton_Lindsay_Functions_Assignment/js/script.js
@@ -8,3 +8,8 @@
 //alert("Testing 1, 2, 3...");
 
 // Finding the cost of a product plus tax.
+
+// Create variables for original price and tax.
+var origPrice = prompt("Let's calculate the total price of your next purchase.\nWhat is the original price of the product?\nPlease leave out the dollar sign.");
+var taxAmount = prompt("Please enter the percentage of sales tax.");
+
